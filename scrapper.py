4c9d7ce840746c6eb106e21c5d7c09aa0edb68ef
kadobosz <s219630@student.uek.krakow.pl>
2021-05-30 16:33:52 +0200

    2021-05-30 KD

--- scrapper.py
+++ scrapper.py
@@ -1,25 +1,55 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.ceneo.pl/71299209#tab=reviews')
 
-page_dom = BeautifulSoup(response.text, "html.parser")
+def extract_element(dom_tree, selector, attribute=None):
+    try:
+        if isinstance(attribute, str):
+            return dom_tree.select_one(selector)[attribute].strip()
 
-reviews = page_dom.select("div.js_product-review")
+        if isinstance(attribute, list):
+            return [e.text.strip() for e in dom_tree.select(selector)]
 
-review = reviews.pop(0)
+        return dom_tree.select_one(selector).text.strip()
 
-review_id = review["data-entry-id"]
-author = review.select_one("span.user-post__author-name").text.strip()
-recommendation = review.select_one("span.user-post__author-recomendation").text.strip()
-stars = review.select_one("span.user-post__score-count").text.strip()
-content = review.select_one("div.user-post__text").text.strip()
-pros = review.select("div.review-feature__title--positives ~ div.review-feature__item")
-cons = review.select("div.review-feature__title--negatives ~ div.review-feature__item")
-useful = review.select_one("button.vote-yes > span").text.strip() 
-useless = review.select_one("button.vote-no > span").text.strip()
-purchased = review.select_one("div.review-pz").text.strip() 
-review_date = review.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
-purchase_date = review.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
+    except (AttributeError, TypeError):
+        return None
 
-print(review_id,author,recommendation,stars,content,pros,cons,useful,useless,purchased,review_date,purchase_date)
+
+all_reviews = []
+
+service_url = 'https://www.ceneo.pl'
+next_page = '/39562616#tab=reviews'
+
+while next_page:
+    #response = requests.get('https://www.ceneo.pl/71299209#tab=reviews')
+    response = requests.get(service_url + next_page)
+
+    page_dom = BeautifulSoup(response.text, "html.parser")
+
+    reviews = page_dom.select("div.js_product-review")
+
+    for review in reviews:
+        single_review = {
+            'review_id' : review["data-entry-id"],
+            'author' : extract_element(review, "span.user-post__author-name"),
+            'recommendation' : extract_element(review,"span.user-post__author-recomendation"),
+            'stars' : extract_element(review,"span.user-post__score-count"),
+            'content' : extract_element(review,"div.user-post__text"),
+            'pros' : extract_element(review,"div.review-feature__title--positives ~ div.review-feature__item", []),
+            'cons' : extract_element(review,"div.review-feature__title--negatives ~ div.review-feature__item", []),
+            'useful' : extract_element(review,"button.vote-yes > span") ,
+            'useless' : extract_element(review,"button.vote-no > span"),
+            'purchased' : extract_element(review,"div.review-pz"),
+            'review_date' : extract_element(review, "span.user-post__published > time:nth-child(1)" ,"datetime"),
+            'purchase_date' : extract_element(review, "span.user-post__published > time:nth-child(2)" ,"datetime"),
+        }
+        all_reviews.append(single_review)
+
+    next_page = extract_element(page_dom, "a.pagination_next", "href")
+
+with open("opinions/39562616.json", "w", encoding="UTF-8") as json_file:
+    json.dump(all_reviews, json_file, ensure_ascii=False,indent=4)
+
+#print(json.dumps(all_reviews, ensure_ascii=False, indent=4))
